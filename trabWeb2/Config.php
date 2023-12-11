c5f780eb7e7f749e97c17575ec9e222d129d724a
José Gabriel <112900256+Olotum@users.noreply.github.com>
2023-12-11 15:30:03 -0300

    Update Config.php

--- trabWeb2/Config.php
+++ trabWeb2/Config.php
@@ -4,4 +4,4 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 define("DSN", "mysql:host=localhost:3306;dbname=team_mk");
 define("USER", "root");
-define("PASSWORD", "Jose01/01/203@");
+define("PASSWORD", "***");
